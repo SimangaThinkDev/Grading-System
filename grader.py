7170074a99f6e68f9ee0558613ba139230b7a022
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-02-14 07:39:20 +0200

    Libraries added

---
+++ grader.py
@@ -0,0 +1,8 @@
+import smtplib
+import os, time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
